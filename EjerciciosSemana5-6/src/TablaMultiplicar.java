dacb4832a40f8b5a52dd84a33d5df5c37f289507
Gonzalo <gonzagmmp@gmail.com>
2025-12-18 12:12:18 +0100

    Ejercicio tabla de multiplicar con bucle for
    Primer ejercicio de bucles - Semana 5

--- EjerciciosSemana5-6/src/TablaMultiplicar.java
+++ EjerciciosSemana5-6/src/TablaMultiplicar.java
@@ -1,22 +1,20 @@
+import java.util.Scanner;
+
 public class TablaMultiplicar {
 
-    import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
-    public class TablaMultiplicar {
-        public static void main(String[] args) {
-            Scanner teclado = new Scanner(System.in);
+        System.out.print("¿De qué número quieres ver la tabla? ");
+        int numero = teclado.nextInt();
 
-            System.out.print("¿De qué número quieres ver la tabla? ");
-            int numero = teclado.nextInt();
+        System.out.println("\n=== Tabla del " + numero + " ===");
 
-            System.out.println("\n=== Tabla del " + numero + " ===");
-
-            for (int i = 1; i <= 10; i++) {
-                int resultado = numero * i;
-                System.out.println(numero + " x " + i + " = " + resultado);
-            }
-
-            teclado.close();
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
         }
+
+        teclado.close();
     }
 }
